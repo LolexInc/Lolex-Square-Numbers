49b3883aafc40499e8868bdb68c4a33b3f0f3704
Monkeyboy2805 <lolexinc2805@gmail.com>
2016-09-13 13:53:29 +0100

    Files

---
+++ Squarensquare.py
@@ -0,0 +1,29 @@
+
+import sys,os,subprocess,io,time
+sys.path.insert(0,"\\")
+try:
+    import squarenumber
+except(ImportError):
+    with io.FileIO("squarenumber.py","w"):
+        pass
+    with open ("squarenumber.py","a") as f: f.write("squarenumbers = 2\noriginsquare = 2")
+    subprocess.call("Squarensquare.py",shell = True)
+    
+import squarenumber
+squarenumbers = squarenumber.squarenumbers
+originsquare = squarenumber.originsquare
+gone = 0
+while True:
+    squarenumbers = originsquare*originsquare
+    originsquare = originsquare + 1
+    print(squarenumbers)
+    #time.sleep(0.2)
+    gone = gone + 1
+    if gone == 1000 or gone>1000:
+        os.remove("squarenumber.py")
+        with open ("squarenumber.py","a") as f: f.write("squarenumbers = ")
+        with open ("squarenumber.py","a") as outf: outf.write(str(squarenumbers))
+        with open ("squarenumber.py","a") as f: f.write("\noriginsquare = ")
+        with open ("squarenumber.py","a") as outf: outf.write(str(originsquare))
+        gone = 0
+
---
+++ SquarensquareCI.py
@@ -0,0 +1,12 @@
+import sys,os,subprocess,io,time
+
+    
+import squarenumber
+squarenumbers = 2
+originsquare = 2
+gone = 0
+while gone != 1000:
+    squarenumbers = originsquare*originsquare
+    originsquare = originsquare + 1
+    print(squarenumbers)
+    gone = gone + 1
